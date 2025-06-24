709cb3a8e7a47152fe3afcdd20ea8988648be99d
lubiano83 <josepablolubiano@lubiano83-2.local>
2025-06-24 14:41:32 -0400

    ajustes varios

--- app/components/admin/Admin.js
+++ app/components/admin/Admin.js
@@ -5,7 +5,7 @@ export default function Admin() {
 
     return (
         <div className="w-full flex justify-center items-center gap-4">
-            <Link href={"/pages/admin/lodges/add"} className="bg-light px-4 py-2 rounded-xl shadow-sm shadow-gray-700 hover:scale-105">
+            <Link href={"/pages/admin/lodges/create"} className="bg-light px-4 py-2 rounded-xl shadow-sm shadow-gray-700 hover:scale-105">
                 <Message>Agregar</Message>
             </Link>
             <Link href={"/pages/admin/lodges"} className="bg-light px-4 py-2 rounded-xl shadow-sm shadow-gray-700 hover:scale-105">
